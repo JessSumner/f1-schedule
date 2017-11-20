7622a73560b660c700a7a5dd9836452d12b435a7
Jessica Sumner <jess.r.sumner@gmail.com>
2017-11-19 20:52:59 -0500

    Builds out the structure of the app
    - Simple model with just a schedule field which is a string
    - Sets up the getF1Schedule function which makes the http request
    - Sets up a simple decoder which returns a race name
    - Sets up a simple view which just displays the string "Here is your schedule"
    
    This compiles however there is lots more to do.
    
    What I want to be returned by the decoded is a list of string date pairs which are the race name and the date it is on.
    
    The view needs to be built out so you can actually make the request and see the result.
    
    Once it is possible to make the request for the hardcoded year this can be extended to be any chosen year.
    
    Maybe this can also be extended to show last and next race, not just the whole schedule.

--- Main.elm
+++ Main.elm
@@ -1,18 +1,73 @@
 module Schedule exposing (..)
 
 import Html exposing (text)
+import Http
+import Json.Decode exposing (..)
 
+main : Program Never Model Msg
 main =
-  text "Here is your schedule"
-
+    Html.program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = always Sub.none
+        }
 type alias Model =
-    { raceName : String
+    { schedule : String
     }
 
-decodeRaceName : Decoder String
-decodeRaceName =
+
+type Msg
+    = GetF1Schedule
+    | GotF1Schedule ( Result Http.Error String )
+    | NoOp
+
+init : ( Model, Cmd Msg )
+init =
+    update GetF1Schedule { schedule = "none" }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        GetF1Schedule ->
+            ( model, Http.send GotF1Schedule getF1Schedule )
+
+        GotF1Schedule result ->
+            case result of
+                Err httpError ->
+                    let
+                        _ =
+                            Debug.log "http error getting the schedule" httpError
+                    in
+                        ( model, Cmd.none )
+
+                Ok schedule ->
+                    ( { model | schedule = schedule }, Cmd.none)
+
+api : String
+api = 
+    "http://ergast.com/api/f1/2017.json"
+
+getF1Schedule : Http.Request String
+getF1Schedule =
+    Http.get api decodeResult
+
+
+decodeResult : Decoder String
+decodeResult =
     at [ "RaceTable", "Races", "raceName" ] string
-{-
+
+
+view : Model -> Html.Html Msg
+view model =
+    Html.text "Here is your schedule"
+
+{--
+Example of the json data
 {
 "MRData": {
     "xmlns": "http:\/\/ergast.com\/mrd\/1.0",
@@ -50,4 +105,4 @@ decodeRaceName =
     }
   }
 }
--}
+--}
